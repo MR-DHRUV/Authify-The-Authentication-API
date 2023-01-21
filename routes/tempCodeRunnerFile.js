15cdef02e374ffab26e62e39aa00120c266a66ec
MR-DHRUV <dhruvgupta742@gmail.com>
2023-01-21 22:34:43 +0530

    Redundant files removed

--- routes/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-transporter.sendMail(mailOptions, function(error, info){
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Email sent: ' + info.response);
-//   }
-// }); 
